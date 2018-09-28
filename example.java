14fb04385a7320a89f34af3a081dab569a1c2d63
Nandhini15 <nandhininandhu94@gmail.com>
2018-09-27 23:33:09 -0400

    Add files via upload
    I tried to commit the whole folder by creating a clone of existing files to a local folder and to merge the changes from my end, then to commit. But, when i tried cloning I received a error message as
    "$ git clone https://github.com/GunpreetAhuja/BugPatterns.git
    Cloning into 'BugPatterns'...
    remote: Repository not found.
    fatal: repository 'https://github.com/GunpreetAhuja/BugPatterns.git/' not found"
    So, I just commited the individual files.

---
+++ example.java
@@ -0,0 +1,19 @@
+import java.util.HashMap;
+
+public class example {
+
+	    public static void main(String[] args) {
+
+	        // Test true and false booleans.
+	        boolean value = true;
+	        if (true) {
+	            System.out.println("A");
+	        }
+	        value = false;
+	        if (!value) {
+	            System.out.println("B");
+	        }
+	    }
+	
+
+}
