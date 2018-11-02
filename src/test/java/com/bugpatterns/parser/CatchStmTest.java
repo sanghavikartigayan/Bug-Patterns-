9950732062c94220eabcae62c2aa73dc09db5457
Gurpreetlotey <gurpreet.rajarh@gmail.com>
2018-11-02 16:18:30 -0400

    Add files via upload

--- src/test/java/com/bugpatterns/parser/CatchStmTest.java
+++ src/test/java/com/bugpatterns/parser/CatchStmTest.java
@@ -8,17 +8,17 @@ import junit.framework.TestCase;
 public class CatchStmTest extends TestCase {
 	
 	protected CatchClauseStm testCatchClauseStm = new CatchClauseStm();
+	protected String dirPath = null;
 	
 	protected void setUp() throws IOException {
 		File dirs = new File("." );
-		String dirPath = dirs.getCanonicalPath() + File.separator+"src"+File.separator+"resources"+File.separator; 
-		CatchClauseStm catchClauseStm = new CatchClauseStm();
-		catchClauseStm.getTryStm(new File(dirPath));
-		catchClauseStm.getConsecutiveCatchDuplStms();
+		dirPath = dirs.getCanonicalPath() + File.separator+"src"+File.separator+"resources"+File.separator; 
+		
+		
     }
 	
 	 public void testSize() {
-	        assertEquals("Count of error", 4, testCatchClauseStm.getConsecutiveCatchDuplStms());
+	        assertEquals("Count of error", 10, testCatchClauseStm.getDupCatchStm(new File(dirPath)));
 	    }
 
 
